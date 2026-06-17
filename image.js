16f22d2740ba6b6c36a6e6728b63925b15065f3a
bodalciouz <carlayrtoncrespo7205@gmail.com>
2026-06-17 14:51:34 +0800

    updated student modules

--- image.js
+++ image.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () =>{
     const uploadBoxes = document.querySelectorAll(".upload-box");
 
-    uploadBoxes.forEach(box => {
+    uploadBoxes.forEach(box =>{
         const input = box.querySelector("input[type='file']");
         const img = box.querySelector(".preview-image");
         const text = box.querySelector(".upload-text");
 
-        input.addEventListener("change", function () {
+        input.addEventListener("change", function (){
             const file = this.files[0];
             if (!file) return;
 
             const reader = new FileReader();
 
-            reader.onload = function (e) {
+            reader.onload = function (e){
                 img.src = e.target.result;
                 img.style.display = "block";
                 text.style.display = "none";
@@ -20,5 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
             reader.readAsDataURL(file);
         });
+
+        box.resetPreview = function (){
+            input.value = "";
+            img.src = "";
+            img.style.display = "none";
+            text.style.display = "block";
+        };
     });
 });
