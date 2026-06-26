a020918d043dd6a445cf0763ea311d474e209cd9
Marc <mlquizon8@gmail.com>
2026-06-26 14:01:19 +0800

    Carousell Image applied, and Angelo's style added to all list pages

--- javascript/admin/staffadmin_lists.js
+++ javascript/admin/staffadmin_lists.js
@@ -44,6 +44,37 @@ const images = document.querySelectorAll('.ImgMatches');
     })
 //JAVASCRIPT FOR DROPDOWN INFO IN IMAGE MATCHES
 
+//JAVASCRIPT FOR CAROUSELL IMAGE
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+//JAVASCRIPT FOR CAROUSELL IMAGE
+
 //JAVASCRIPT FOR TOAST MESSAGES
 const toastBox = document.getElementById('toastBox');
 
