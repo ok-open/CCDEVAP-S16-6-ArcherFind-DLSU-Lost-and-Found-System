6738cbe74920388a8aa3869999b070f68af5822d
bodalciouz <carlayrtoncrespo7205@gmail.com>
2026-07-15 17:01:41 +0800

    student modules now have links to dashboard

--- pages/student/student_contact.php
+++ pages/student/student_contact.php
@@ -61,7 +61,11 @@
                                     </button></div>
                                 <div class="log-out"><button type="button">
                                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"> <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /> </svg>Log Out</button></div>
-                                <div class="view-dashboard"><button type="button">View Dashboard</button></div>
+                                <div class="view-dashboard">
+                                    <button type="button" onclick="window.location.href='student_dashboard.php'">
+                                        View Dashboard
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -103,7 +107,11 @@
                             </button></div>
                         <div class="log-out"><button type="button">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"> <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /> </svg>Log Out</button></div>
-                        <div class="view-dashboard"><button type="button">View Dashboard</button></div>
+                        <div class="view-dashboard">
+                            <button type="button" onclick="window.location.href='student_dashboard.php'">
+                                View Dashboard
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
--- pages/student/student_manage-account.php
+++ pages/student/student_manage-account.php
@@ -75,7 +75,11 @@ require_once "../../controllers/StudentAuth.php";
                                             <path
                                                 d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
                                         </svg>Log Out</button></div>
-                                <div class="view-dashboard"><button type="button">View Dashboard</button></div>
+                                <div class="view-dashboard">
+                                    <button type="button" onclick="window.location.href='student_dashboard.php'">
+                                        View Dashboard
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -134,7 +138,11 @@ require_once "../../controllers/StudentAuth.php";
                                     <path
                                         d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
                                 </svg>Log Out</button></div>
-                        <div class="view-dashboard"><button type="button">View Dashboard</button></div>
+                        <div class="view-dashboard">
+                            <button type="button" onclick="window.location.href='student_dashboard.php'">
+                                View Dashboard
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
